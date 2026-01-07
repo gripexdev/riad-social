b9c19c749e712584a38f9f57a3ec3246103b597c
gripexdev <cj.othman@gmail.com>
2026-01-07 01:15:34 +0100

    + profile hub card for logged-in users

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { Post, PostService } from '../post/post.service';
 import { PostCardComponent } from '../post/post-card/post-card.component';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -14,11 +15,15 @@ import { RouterLink } from '@angular/router';
 export class HomeComponent implements OnInit {
   posts: Post[] = [];
 
-  constructor(private postService: PostService) { }
+  constructor(private postService: PostService, private authService: AuthService) { }
 
   ngOnInit(): void {
     this.postService.getPosts().subscribe(posts => {
       this.posts = posts;
     });
+  }
+
+  get currentUsername(): string | null {
+    return this.authService.getUsername();
   }
 }
